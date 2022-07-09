45ead3ff0dc8d1080745a9e8ec7899c6b40960e5
Oleksandr Leiberiuk <lejberyk.ua@gmail.com>
2022-07-09 12:16:46 +0300

    Updates

--- static/js/129.e51a1e57.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpaleosoil=self.webpackChunkpaleosoil||[]).push([[129],{2129:function(e,n,u){u.r(n);var l=u(184);n.default=function(){return(0,l.jsx)("div",{children:"NotFound"})}}}]);
-//# sourceMappingURL=129.e51a1e57.chunk.js.map
