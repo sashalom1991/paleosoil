4dba8822d2de205d32d8f191153fa807f2fbbdf9
Oleksandr Leiberiuk <lejberyk.ua@gmail.com>
2022-09-26 19:08:00 +0300

    change desc

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -11,14 +11,13 @@ function Description() {
           <div>
             <h1 className="DescTitle">{t('desc.title')}</h1>
             <p className="DescText">
-              {t('desc.text1')}
+              {t('desc.text1')}. {t('desc.text2')}
               <a
                 href="https://qgiscloud.com/paleogeo_ua/Paleogeographic_studies/?bl=ocm_landscape&st=&l=%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%BD%D1%96%20%D0%B7%D0%BE%D0%BD%D0%B8%2C%D0%91%D0%B0%D0%B7%D0%B0%20%D0%B4%D0%BE%D1%81%D0%BB%D1%96%D0%B4%D0%B6%D0%B5%D0%BD%D1%8C&t=Paleogeographic_studies&e=1488176%2C5079707%2C4874843%2C7437144"
                 alt="Paleomap in Qgis Cloud"
               >
-                QGIS Cloud
+                QGIS Cloud.
               </a>
-              . {t('desc.text2')}
             </p>
           </div>
           <img
